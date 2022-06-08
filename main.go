a800907212944b4ed9de401c897e5934b54c2cd4
ss <setrotec@outlook.com>
2022-06-08 17:09:55 -0300

    Outras formas de trabalhar com struct

--- main.go
+++ main.go
@@ -11,6 +11,28 @@ type ContaCorrente struct {
 
 func main() {
 	contaCorrente01 := ContaCorrente{titular: "Emmanuel", numeroAgencia: 01, numeroConta: 12123, saldo: 2000}
+	contaCorrente04 := ContaCorrente{titular: "Emmanuel", numeroAgencia: 01, numeroConta: 12123, saldo: 2000}
 	contaCorrente02 := ContaCorrente{"Saci Perere", 01, 12133, 4000}
 	fmt.Println(contaCorrente01, contaCorrente02)
+
+	//comprando conetudo e endereços das structs
+	fmt.Println(contaCorrente01 == contaCorrente04)
+
+	contaCorrente03 := new(ContaCorrente)
+	contaCorrente03.titular = "Juca Pirama"
+	contaCorrente03.numeroAgencia = 01
+	contaCorrente03.numeroConta = 10102
+	contaCorrente03.saldo = 10000
+
+	contaCorrente05 := new(ContaCorrente)
+	contaCorrente05.titular = "Juca Pirama"
+	contaCorrente05.numeroAgencia = 01
+	contaCorrente05.numeroConta = 10102
+	contaCorrente05.saldo = 10000
+
+	//comprando conetudo e endereços das structs
+	fmt.Println(contaCorrente03 == contaCorrente05)
+	fmt.Println(*contaCorrente03 == *contaCorrente05)
+	fmt.Println(&contaCorrente03, &contaCorrente05)
+
 }
